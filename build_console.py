a7564c1b5d7330f6b7a8e2f62c9e204f9a98bee1
Lakay <194147183+Lakayy@users.noreply.github.com>
2025-05-27 02:27:10 +0200

    Compiler
    Requirement: PyInstaller

---
+++ build_console.py
@@ -0,0 +1,16 @@
+import PyInstaller.__main__
+import os
+
+# Get the absolute path of the script directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+PyInstaller.__main__.run([
+    'v1.1.py',
+    '--name=Lakay Shelter',
+    '--onefile',
+    '--console',  # Console mode since it's a CLI application
+    '--icon=shelter.ico',  # You'll need to create this icon
+    f'--workpath={os.path.join(script_dir, "build")}',
+    f'--distpath={os.path.join(script_dir, "dist")}',
+    '--clean',
+]) 
